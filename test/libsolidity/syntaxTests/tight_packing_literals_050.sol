08eadeec79f8576ff2d364c6520c8b8f33eea2bc
Alex Beregszaszi <alex@rtfs.hu>
2018-04-20 01:55:50 +0100

    Turn deprecated warnings for sha3/suicide into errors (experimental 0.5.0)

--- test/libsolidity/syntaxTests/tight_packing_literals_050.sol
+++ test/libsolidity/syntaxTests/tight_packing_literals_050.sol
@@ -19,7 +19,7 @@ contract C {
 
 // ----
 // TypeError: (117-118): The type of "int_const 1" was inferred as uint8. This is probably not desired. Use an explicit type.
-// Warning: (191-198): "sha3" has been deprecated in favour of "keccak256"
+// TypeError: (191-198): "sha3" has been deprecated in favour of "keccak256"
 // TypeError: (196-197): The type of "int_const 1" was inferred as uint8. This is probably not desired. Use an explicit type.
 // TypeError: (277-278): The type of "int_const 1" was inferred as uint8. This is probably not desired. Use an explicit type.
 // TypeError: (361-362): The type of "int_const 1" was inferred as uint8. This is probably not desired. Use an explicit type.
